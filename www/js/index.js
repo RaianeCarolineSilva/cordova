8c61d5a5c3c1ba5b90d0e1c4cb475ec379269b7d
Raiane <poscarayane@gmail.com>
2024-04-26 22:35:43 -0300

    cordova atualizado

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,25 @@
 function consultaCep()
 {
+  let campo = document.querySelector("#cep");
   let requisicao = new XMLHttpRequest();
-  let url = "https://viacep.com.br/ws/01001000/json/";
+  let url = "https://viacep.com.br/ws/" + campo.value + "/json/";
   requisicao.open("GET" , url);
   requisicao.send();
   requisicao.onload = function(){
-        alert(JSON.parse(requisicao.response).logradouro);
-  }
+    let objetoCEP = JSON.parse(requisicao.response)
+    let campoEndereco = document.querySelector("#logradouro")
+    let campoComplemento = document.querySelector("#complemento")
+    let campoBairro = document.querySelector("#bairro")
+    let campoLocalidade = document.querySelector("#localidade")
+
+
+    campoComplemento.value = objetoCEP.complemento
+    campoEndereco.value = objetoCEP.logradouro
+    campoBairro.value = objetoCEP.bairro
+    campoLocalidade.value = objetoCEP.localidade
+
+   }
+
 }
 
 let botao = document.querySelector("#botao");
